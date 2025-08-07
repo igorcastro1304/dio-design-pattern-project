d89a703e36492e4e26021cc7149d3c216d1266e7
igorcastro1304 <igor.hoffmann@al.infnet.edu.br>
2025-08-06 22:02:45 -0400

    First commit

---
+++ src/main/java/br/com/dio/dio_design_pattern_project/Main.java
@@ -0,0 +1,13 @@
+package br.com.dio.dio_design_pattern_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
---
+++ src/test/java/br/com/dio/dio_design_pattern_project/DioDesignPatternProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.dio_design_pattern_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesignPatternProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
